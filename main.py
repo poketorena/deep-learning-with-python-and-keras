be8de0e807ddc3526ec580a56149b60aea4f8d86
poketorena <poketorena@users.noreply.github.com>
2019-07-02 11:28:38 +0900

    [update] 行列、2次元テンソル

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-x = np.array([12, 3, 6, 14, 7])
+x = np.array([[5, 78, 2, 34, 0],
+              [5, 78, 2, 34, 1],
+              [5, 78, 2, 34, 2]])
 
 print(x)
 print(x.ndim)
