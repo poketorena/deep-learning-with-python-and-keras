96b0d7345925c891b57c02912741c76566dba8ff
poketorena <poketorena@users.noreply.github.com>
2019-07-02 17:11:56 +0900

    [update] IMDbデータセットの読み込み

--- main.py
+++ main.py
@@ -1,56 +1,24 @@
-from keras.datasets import mnist
+from keras.datasets import imdb
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
-print("train_images.shape", train_images.shape)
-print("len(train_labels)", len(train_labels))
-print("train_labels", train_labels)
+print(train_data[0])
+print(train_labels[0])
 
-print("test_images", test_images.shape)
-print("len(test_labels)", len(test_labels))
-print("test_labels", test_labels)
+print(max([max(sequence) for sequence in train_data]))
 
-from keras import models
-from keras import layers
+# word_indexは単語を整数のインデックスにマッピングする辞書
+word_index = imdb.get_word_index()
 
-# Denseは全結合層（普通のニューラルネットワーク）のこと
-# network = models.Sequential()
-# network.add(layers.Dense(512, activation="relu", input_shape=(28 * 28,)))
-# network.add(layers.Dense(10, activation="softmax"))
-# model = network
+# 整数のインデックスを単語にマッピング
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
-# Functional APIを使って書く場合
-input_tensor = layers.Input(shape=(784,))
-x = layers.Dense(512, activation="relu")(input_tensor)
-output_tensor = layers.Dense(10, activation="softmax")(x)
+# レビューを出コード：インデックスのオフセットとして3が指定されているのは、
+# 0、1、2がそれぞれ「パディング」、「シーケンスの開始」、「不明」の
+# インデックスとして予約されていることであることに注意
+decoded_review = " ".join(
+    [reverse_word_index.get(i - 3, "?") for i in train_data[0]]
+)
 
-model = models.Model(inputs=input_tensor, outputs=output_tensor)
-
-# モデルをコンパイルする
-
-from keras import optimizers
-
-model.compile(optimizer=optimizers.RMSprop(lr=0.001),
-              loss="mse",
-              metrics=["accuracy"])
-
-# 0-1の値に変換する
-train_images = train_images.reshape((60000, 28 * 28))
-train_images = train_images.astype("float32") / 255
-
-test_images = test_images.reshape((10000, 28 * 28))
-test_iamges = test_images.astype("float32") / 255
-
-# ラベルをカテゴリ値でエンコードする
-from keras.utils import to_categorical
-
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
-
-# 訓練する
-model.fit(train_images, train_labels, epochs=5, batch_size=128)
-
-# テストデータで検証する
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print("test_loss:", test_loss)
-print("test_acc:", test_acc)
+# デコードしたレビューの内容を表示
+print(decoded_review)
