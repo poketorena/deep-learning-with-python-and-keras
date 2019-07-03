a732cd86c0062358e8cc743a1b7c251be4b017b8
poketorena <poketorena@users.noreply.github.com>
2019-07-03 23:55:20 +0900

    [update] 住宅価格の予測（ニューラルネットワークの構築）

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from keras import models
 from keras.datasets import boston_housing
+from keras import layers
 
 (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
 
@@ -19,4 +21,14 @@ train_data /= std
 test_data -= mean
 test_data /= std
 
-print()
+
+def build_model():
+    # 同じモデルを複数回インスタンス化するため
+    # モデルをインスタンス化するための関数を使用
+    model = models.Sequential()
+    model.add(layers.Dense(64, activation="relu",
+                           input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation="relu"))
+    model.add(layers.Dense(1))
+    model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
+    return model
