166f8b8d2f91150285457757f8c77c0880e9b778
poketorena <poketorena@users.noreply.github.com>
2019-07-03 14:10:09 +0900

    [update] 多クラス単一ラベル分類（ニュースサンプルをテキストに変換する）

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from keras.datasets import reuters
 
-(train_data,train_labels),(test_data,test_labels)=reuters.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 
 print(len(train_data))
 print(len(test_data))
 print(train_data[10])
 
+word_index = reuters.get_word_index()
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+# インデックスのオフセットとして3が指定されているのは
+# 0、1、2がそれぞれ「パディング」、「シーケンスの開始」、「不明」の
+# インデックスとして予約されているためであることに注意
+decoded_newswire = " ".join(
+    [reverse_word_index.get(i - 3, "?") for i in train_data[0]]
+)
+
+# デコードしたニュースを表示
+print(decoded_newswire)
