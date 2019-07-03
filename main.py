4ecf5b546dff59f60030a4b2c2d522e5952a376c
poketorena <poketorena@users.noreply.github.com>
2019-07-03 15:39:24 +0900

    [update] 多クラス単一ラベル分類（学習1、8エポックの後、過学習する）

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
 from keras.datasets import reuters
 from keras.utils.np_utils import to_categorical
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
 
 
 def vectorize_sequences(sequences, dimension=10000):
@@ -52,10 +55,62 @@ one_hot_train_labels1 = to_one_hot(train_labels)
 one_hot_test_labels1 = to_one_hot(test_labels)
 
 # Kerasの機能を使ってone-hotエンコーディング（カテゴリエンコーディング）した場合
-one_hot_train_labels2 = to_categorical(train_labels)
-one_hot_test_labels2 = to_categorical(test_labels)
+# one_hot_train_labels2 = to_categorical(train_labels)
+# one_hot_test_labels2 = to_categorical(test_labels)
 
 # 自作関数でone-hotエンコーディングした結果がKerasの関数を使った場合と同じ結果になる
 # 今後はKerasの関数を使おう
-print((one_hot_test_labels1 == one_hot_test_labels2).all())
-print((one_hot_train_labels1 == one_hot_train_labels2).all())
+# print((one_hot_test_labels1 == one_hot_test_labels2).all())
+# print((one_hot_train_labels1 == one_hot_train_labels2).all())
+
+# モデルの作成
+model = models.Sequential()
+model.add(layers.Dense(64, activation="relu", input_shape=(10000,)))
+model.add(layers.Dense(64, activation="relu"))
+model.add(layers.Dense(46, activation="softmax"))
+
+model.compile(optimizer="rmsprop",
+              loss="categorical_crossentropy",
+              metrics=["accuracy"])
+
+# 検証データセットの設定
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+
+y_val = one_hot_train_labels1[:1000]
+partial_y_train = one_hot_train_labels1[1000:]
+
+# モデルの訓練
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+# 訓練データと検証データでの損失値をプロット
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, "bo", label="Training loss")
+plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.title("Training and validation loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+# 訓練データと検証データでの正解率のプロット
+plt.clf()  # 図を消去
+
+acc = history.history["acc"]
+val_acc = history.history["val_acc"]
+
+plt.plot(epochs, acc, "bo", label="Training acc")
+plt.plot(epochs, val_acc, "b", label="Validation acc")
+plt.title("Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
