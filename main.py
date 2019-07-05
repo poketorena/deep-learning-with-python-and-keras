4a9595cd56f1a1cc5ba86e2b99cd1f172fcc4975
poketorena <poketorena@users.noreply.github.com>
2019-07-05 23:06:29 +0900

    [update] MNISTをCNNで解いてみる（モデルをKeras Functional APIで書いた場合）

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from keras import models
+from keras.models import Model
+from keras import Input
 from keras.datasets import mnist
 from keras import layers
 from keras.utils import to_categorical
@@ -14,15 +15,28 @@ test_images = test_images.astype("float32") / 255
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)))
-model.add(layers.MaxPool2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-model.add(layers.MaxPool2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation="relu"))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation="relu"))
-model.add(layers.Dense(10, activation="softmax"))
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)))
+# model.add(layers.MaxPool2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+# model.add(layers.MaxPool2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation="relu"))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation="relu"))
+# model.add(layers.Dense(10, activation="softmax"))
+
+# モデルをKeras Functional APIで書いた場合
+input_tensor = Input(shape=(28, 28, 1))
+x = layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu")(input_tensor)
+x = layers.MaxPool2D(pool_size=(2, 2))(x)
+x = layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu")(x)
+x = layers.MaxPool2D(pool_size=(2, 2))(x)
+x = layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu")(x)
+x = layers.Flatten()(x)
+x = layers.Dense(units=64, activation="relu")(x)
+output_tensor = layers.Dense(units=10, activation="softmax")(x)
+
+model = Model(input_tensor, output_tensor)
 
 model.compile(optimizer="rmsprop",
               loss="categorical_crossentropy",
