bfcf8d81b05a05d316294a9990353eb6b4871bde
poketorena <poketorena@users.noreply.github.com>
2019-07-06 17:17:50 +0900

    [update] パッチジェネレータを使ってモデルを適合する（猫の画像のみで学習していたので犬と猫両方の画像で学習するよう修正）
    猫画像だけで犬と猫に分けようとしたらうまくいかないのは当たり前だよね

--- main.py
+++ main.py
@@ -10,81 +10,81 @@ original_dataset_dir = "./dogs-vs-cats/train"
 
 # より小さなデータセットを格納するディレクトリへのパス
 base_dir = "./cats-and-dogs-small"
-# os.mkdir(base_dir)
+os.mkdir(base_dir)
 
 # 訓練データセット、検証データセット、テストデータセットを配置するディレクトリ
 train_dir = os.path.join(base_dir, "train")
-# os.mkdir(train_dir)
+os.mkdir(train_dir)
 validation_dir = os.path.join(base_dir, "validation")
-# os.mkdir(validation_dir)
+os.mkdir(validation_dir)
 test_dir = os.path.join(base_dir, "test")
-# os.mkdir(test_dir)
+os.mkdir(test_dir)
 
 # 訓練用の猫の画像を配置するディレクトリ
 train_cats_dir = os.path.join(train_dir, "cats")
-# os.mkdir(train_cats_dir)
+os.mkdir(train_cats_dir)
 
 # 訓練用の犬の画像を配置するディレクトリ
 train_dogs_dir = os.path.join(train_dir, "dogs")
-# os.mkdir(train_dogs_dir)
+os.mkdir(train_dogs_dir)
 
 # 検証用の猫の画像を配置するディレクトリ
 validation_cats_dir = os.path.join(validation_dir, "cats")
-# os.mkdir(validation_cats_dir)
+os.mkdir(validation_cats_dir)
 
 # 検証用の犬の画像を配置するディレクトリ
 validation_dogs_dir = os.path.join(validation_dir, "dogs")
-# os.mkdir(validation_dogs_dir)
+os.mkdir(validation_dogs_dir)
 
 # テスト用の猫の画像を配置するディレクトリ
 test_cats_dir = os.path.join(test_dir, "cats")
-# os.mkdir(test_cats_dir)
+os.mkdir(test_cats_dir)
 
 # テスト用の犬の画像を配置するディレクトリ
 test_dogs_dir = os.path.join(test_dir, "dogs")
-# os.mkdir(test_dogs_dir)
+os.mkdir(test_dogs_dir)
 
 # 最初の1000個の猫画像をtrain_cats_dirにコピー
 fnames = [f"cat.{i}.jpg" for i in range(1000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(train_cats_dir, fname)
-    # shutil.copyfile(src, dst)
+    shutil.copyfile(src, dst)
 
 # 次の500個の猫画像をvalidation_cats_dirにコピー
 fnames = [f"cat.{i}.jpg" for i in range(1000, 1500)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(validation_cats_dir, fname)
-    # shutil.copyfile(src, dst)
+    shutil.copyfile(src, dst)
 
 # 次の500個の猫画像をtest_cats_dirにコピー
 fnames = [f"cat.{i}.jpg" for i in range(1500, 2000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(test_cats_dir, fname)
-    # shutil.copyfile(src, dst)
+    shutil.copyfile(src, dst)
 
-# 最初の1000個の猫画像をtrain_dogs_dirにコピー
-fnames = [f"cat.{i}.jpg" for i in range(1000)]
+# 最初の1000個の犬画像をtrain_dogs_dirにコピー
+fnames = [f"dog.{i}.jpg" for i in range(1000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(train_dogs_dir, fname)
-    # shutil.copyfile(src, dst)
+    shutil.copyfile(src, dst)
 
-# 次の500個の猫画像をvalidation_dogs_dirにコピー
-fnames = [f"cat.{i}.jpg" for i in range(1000, 1500)]
+# 次の500個の犬画像をvalidation_dogs_dirにコピー
+fnames = [f"dog.{i}.jpg" for i in range(1000, 1500)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(validation_dogs_dir, fname)
-    # shutil.copyfile(src, dst)
+    shutil.copyfile(src, dst)
 
-# 次の500個の猫画像をtest_dogs_dirにコピー
-fnames = [f"cat.{i}.jpg" for i in range(1500, 2000)]
+# 次の500個の犬画像をtest_dogs_dirにコピー
+fnames = [f"dog.{i}.jpg" for i in range(1500, 2000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
     dst = os.path.join(test_dogs_dir, fname)
-    # shutil.copyfile(src, dst)
+    shutil.copyfile(src, dst)
 
 # コピーが成功したかチェックする（健全性チェック）
 print("total training cat images:", len(os.listdir(train_cats_dir)))
