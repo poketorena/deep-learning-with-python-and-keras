b090f0a67c1fbca4174752bd6c40852fcd670127
poketorena <poketorena@users.noreply.github.com>
2019-07-07 21:51:54 +0900

    [update] 犬と猫を分類するプログラム（ImageNet VGG16から転移学習を行い、データを水増しして分類用の全結合層のみを学習する場合）

--- main.py
+++ main.py
@@ -104,61 +104,62 @@ conv_base = VGG16(weights="imagenet",
                   include_top=False,
                   input_shape=(150, 150, 3))
 
-datagen = ImageDataGenerator(rescale=1. / 255)
-batch_size = 20
-
-
-def extract_features(directory, sample_count):
-    features = np.zeros(shape=(sample_count, 4, 4, 512))
-    labels = np.zeros(shape=(sample_count))
-    generator = datagen.flow_from_directory(directory,
-                                            target_size=(150, 150),
-                                            batch_size=batch_size,
-                                            class_mode="binary")
-    i = 0
-    for inputs_batch, labels_batch in generator:
-        features_batch = conv_base.predict(inputs_batch)
-        features[i * batch_size:(i + 1) * batch_size] = features_batch
-        labels[i * batch_size:(i + 1) * batch_size] = labels_batch
-        i += 1
-        if i * batch_size >= sample_count:
-            # ジェネレータはデータを無限ループで生成するため
-            # 画像を一通り処理したらbreakしなければならない
-            break
-    return features, labels
-
-
-train_features, train_labels = extract_features(train_dir, 2000)
-validation_features, validation_labels = extract_features(validation_dir, 1000)
-
-test_features, test_labels = extract_features(test_dir, 1000)
-
-# 平坦化する
-train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
-validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
-test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
-
+# モデル
 model = models.Sequential()
-
-model.add(layers.Dense(256, activation="relu", input_dim=4 * 4 * 512))
-model.add(layers.Dropout(0.5))
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation="relu"))
 model.add(layers.Dense(1, activation="sigmoid"))
 
-print(model.summary())
+# VGG16モデルの重みを凍結する
+print()
+print(f"This is he number of trainable weights before freezing the conv base: {len(model.trainable_weights)}")
+conv_base.trainable = False
+print(f"This is he number of trainable weights after freezing the conv base: {len(model.trainable_weights)}")
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode="nearest"
+)
+
+# 検証データは水増しすべきではないことに注意
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode="binary"
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode="binary"
+)
 
 model.compile(loss="binary_crossentropy",
               optimizer=optimizers.RMSprop(lr=2e-5),
               metrics=["acc"])
 
-# モデルを適合
-history = model.fit(train_features,
-                    train_labels,
-                    epochs=30,
-                    batch_size=20,
-                    validation_data=(validation_features, validation_labels))
+print(model.summary())
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50,
+                              verbose=1)
 
 # モデルを保存
-model.save("cats_and_dogs_small_transfer_learning_fit_dense_only.h5")
+model.save("cats_and_dogs_small_transfer_learning_fit_dense_overall_optimization.h5")
 
 # 訓練時の損失値を正解率をプロット
 acc = history.history["acc"]
