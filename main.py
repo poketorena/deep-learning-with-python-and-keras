e67e93562d106a84711e5230b0299ff5d0c1993d
poketorena <poketorena@users.noreply.github.com>
2019-07-08 10:51:41 +0900

    [update] 犬と猫を分類するプログラム（ImageNet VGG16から転移学習を行い、データを水増しして一部の畳み込み層と全結合層をファインチューニングし、 指数移動平均を使ってグラフを滑らかに表示する）

--- main.py
+++ main.py
@@ -197,12 +197,12 @@ for layer in conv_base.layers:
     else:
         layer.trainable = False
 
-print(model.summary())
-
 # モデルのファインチューニング
 model.compile(loss="binary_crossentropy",
               optimizer=optimizers.RMSprop(lr=1e-5),
               metrics=["acc"])
+
+print(model.summary())
 
 history = model.fit_generator(train_generator,
                               steps_per_epoch=100,
@@ -210,7 +210,7 @@ history = model.fit_generator(train_generator,
                               validation_data=validation_generator,
                               validation_steps=50)
 
-# 訓練時の損失値を正解率をプロット
+# 訓練時の損失値と正解率をプロット（指数移動平均を使ってグラフを滑らかにする）
 acc = history.history["acc"]
 val_acc = history.history["val_acc"]
 loss = history.history["loss"]
@@ -218,17 +218,27 @@ val_loss = history.history["val_loss"]
 
 epochs = range(len(acc))
 
-# 正解率をプロット
-plt.plot(epochs, acc, "bo", label="Training acc")
-plt.plot(epochs, val_acc, "b", label="Validation acc")
+
+def smooth_curve(points, factor=0.8):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+
+
+plt.plot(epochs, smooth_curve(acc), "bo", label="Smoothed training acc")
+plt.plot(epochs, smooth_curve(val_acc), "b", label="Smoothed validation acc")
 plt.title("Training and validation accuracy")
 plt.legend()
 
 plt.figure()
 
-# 損失値をプロット
-plt.plot(epochs, loss, "bo", label="Training loss")
-plt.plot(epochs, val_loss, "b", label="Validation loss")
+plt.plot(epochs, smooth_curve(loss), "bo", label="Smoothed training loss")
+plt.plot(epochs, smooth_curve(loss), "b", label="Smoothed validation loss")
 plt.title("Training and validation loss")
 plt.legend()
 
